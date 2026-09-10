1bffff6092a77f20b9834f6310984431fa5af911
Trizah NJeri <trizahn2002@gmail.com>
2026-09-10 10:37:43 +0300

    Added protected expense CRUD routes with pagination and ownership checks"

--- server/app.py
+++ server/app.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_migrate import Migrate
 
 from models import db
+from expenses import expenses_bp
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
-
+app.register_blueprint(expenses_bp)
 db.init_app(app)
 
 
